10cf24a281c3d4f498712ed7361d99c179bcd539
ShegutaIvan[INBO-03-18] <36135768+tps051@users.noreply.github.com>
2019-04-13 20:59:09 +0300

    Add files via upload

---
+++ lab2/lab2v1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int summ(int a, int b);
+int razn(int a, int b);
+int proiz(int a, int b);
+int chast(int a, int b);
+
+int main() {
+	int(*func)(int, int);
+	int a, b;
+	cout << "Введите два числа: ";
+	cin >> a >> b;
+	int answer;
+	cout << "Выберите действие: " << '\n' << "1) Сложение " << '\n' << "2) Вычитание" << '\n' << "3) Умножение " << '\n' << "4) Деление " << endl;
+	cin >> answer;
+	switch (answer)
+	{
+	case 1:
+		func = summ;
+		break;
+	case 2:
+		func = razn;
+		break;
+	case 3:
+		func = proiz;
+		break;
+	case 4:
+		func = chast;
+		break;
+	}
+	int result = func(a, b);
+	if (isnan(result)) {
+		cout << "Деление на ноль!" << endl;
+	}
+	else {
+		cout << "Ответ: " << result << endl;
+	}
+	func = NULL;
+	return 0;
+}
+
+int summ(int a, int b) {
+	return a + b;
+};
+
+int razn(int a, int b) {
+	return a - b;
+};
+
+int proiz(int a, int b) {
+	return a * b;
+};
+
+int chast(int a, int b) {
+	return (float)a / (float)b;
+};
---
+++ lab2/lab2v2.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cmath>
+#include <time.h>
+using namespace std;
+
+int * reverse_array(int * arr, int len);//инвертирование массива
+int * sort_vosr(int * arr, int len);//сортировка по возрастанию
+int * sort_ub(int * arr, int len);//сортировка по убыванию
+
+int* (*action(int*, int))(int* array, int len);//возвращает указатель на функцию в соответствии с резльтатом сравнени€ первого элемента и суммы элементов массива (без него)
+
+int main(int argc, char const *argv[])
+{
+	srand(time(0));
+	int * (*func_arr)(int*, int);
+	int size;
+	cout << "¬ведите длину массива: ";
+	cin >> size;
+	int* array = new int[size];
+	for (int i = 0; i < size; i++) {
+		array[i] = rand() % 100;
+		cout << array[i] << '\t';
+	}
+	cout << '\n';
+	func_arr = action(array, size);
+	array = func_arr(array, size);
+	for (int i = 0; i < size; i++) {
+		cout << array[i] << '\t';
+	}
+	cout << endl;
+	delete[] array;
+	func_arr = NULL;
+	return 0;
+};
+
+int * reverse_array(int * arr, int len) {
+	for (int i = 0; i < len / 2; i++) {
+		swap(arr[i], arr[len - 1 - i]);
+	}
+	return arr;
+};
+
+int * sort_vosr(int * arr, int len) {
+	for (int i = 0; i < len; i++) {
+		for (int j = i + 1; j < len; j++) {
+			if (arr[i] > arr[j]) {
+				swap(arr[i], arr[j]);
+			}
+		}
+	}
+	return arr;
+};
+
+int * sort_ub(int * arr, int len) {
+	for (int i = 0; i < len; i++) {
+		for (int j = i + 1; j < len; j++) {
+			if (arr[i] < arr[j]) {
+				swap(arr[i], arr[j]);
+			}
+		}
+	}
+	return arr;
+};
+
+int* (*action(int* array, int len))(int*, int) {
+	int sum;
+	for (int i = 1; i < len; i++) {
+		sum += array[i];
+	}
+	if (sum == array[0]) {
+		return reverse_array;
+	}
+	else if (sum > array[0]) {
+		return sort_vosr;
+	}
+	else {
+		return sort_ub;
+	}
+}
