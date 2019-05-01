708c5e5a11a3f9fdcd4aa35406a82b9a634898c9
tps051 <36135768+tps051@users.noreply.github.com>
2019-05-01 19:23:22 +0300

    Update lab1v28.cpp

--- newlab1v28/lab1v28.cpp
+++ newlab1v28/lab1v28.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <list>
 #include <conio.h>
 using namespace std;
 
@@ -10,7 +9,7 @@ class Vector {
 public:
 	double x;
 	double y;
-	Vector () {}
+	Vector() {}
 	Vector(double x, double y)
 	{
 		this->x = x;
@@ -18,37 +17,33 @@ public:
 	}
 
 };
-double length(int x, int y) 
+double length(int x, int y)
 {
-	return sqrt(pow(x, 2) + pow(y, 2));
+	return sqrt(pow(x, 2) + pow(y,2));
 }
 int main()
 {
 	vector<string> vectx;
 	string buffx = "";
-	int n;
-	cout << "Amount of vectors: ";
-	cin >> n;
+	cout << "Input amount of vectors: ";
 	getline(cin, buffx);
 	vectx.push_back(buffx);
-	cout << "Input random: ";
-	getline(cin, buffx);
-	vectx.push_back(buffx);
+	int n = stoi(vectx[0]);
 	for (int i = 0; i < n; i++) // Цикл ввода координат векторов
 	{
 		cout << "Vector#" << i + 1 << endl;
 		cout << "	Input x: ";
-		getline(cin, buffx); 
-			vectx.push_back(buffx);  // Добавление элемента в конец вектора
+		getline(cin, buffx);
+		vectx.push_back(buffx);  // Добавление элемента в конец вектора
 		cout << "	Input y: ";
 		getline(cin, buffx);
-			vectx.push_back(buffx);
+		vectx.push_back(buffx);
 	}
 
 	unsigned int vector_size = buffx.size();
 	int num = 1;
-	for (int i = 2; i < n*2+1; i = i + 2) {
-		cout << "Vector #" << num << " = (" << vectx[i] << ";" << vectx[i+1] << ")"<< endl;
+	for (int i = 2; i < n * 2 + 1; i = i + 2) {
+		cout << "Vector #" << num << " = (" << vectx[i-1] << ";" << vectx[i] << ")" << endl;
 		num++;
 	}
 	cout << "--------------------------------" << endl;
@@ -57,13 +52,13 @@ int main()
 	char a[100];
 	for (int i = 2; i < n * 2 + 1; i = i + 2)
 	{
-		int x = stoi(vectx[i]);
-		int y = stoi(vectx[i + 1]);
+		int x = stoi(vectx[i-1]);
+		int y = stoi(vectx[i]);
 		if (length(x, y) > max)
 		{
 			max = length(x, y);
-			count = i/2;
-			
+			count = i / 2;
+
 		}
 	}
 	cout << "Vector #" << count << " has max length = " << max << endl;
