41c7e0ecc2915759c888ef8e509e21d158d3c880
tps051 <36135768+tps051@users.noreply.github.com>
2019-05-01 22:13:54 +0300

    Update lab1v28.cpp

--- newlab1v28/lab1v28.cpp
+++ newlab1v28/lab1v28.cpp
@@ -1,11 +1,11 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
 #include <vector>
 #include <conio.h>
 using namespace std;
 
-class Vector {
+class Vector  // Объявление класса
+{
 public:
 	double x;
 	double y;
@@ -17,18 +17,18 @@ public:
 	}
 
 };
-double length(int x, int y)
+double length(int x, int y)  // Функция, возвращающая длину вектора
 {
 	return sqrt(pow(x, 2) + pow(y,2));
 }
 int main()
 {
-	vector<string> vectx;
-	string buffx = "";
+	vector<string> vectx; // Объявление вектора 
+	string buffx = ""; // Обьявление пустой строки, в которую будут заноситься координаты
 	cout << "Input amount of vectors: ";
 	getline(cin, buffx);
 	vectx.push_back(buffx);
-	int n = stoi(vectx[0]);
+	int n = stoi(vectx[0]); // Считывание цифры из строки и перевод ее в целочисленный тип данных
 	for (int i = 0; i < n; i++) // Цикл ввода координат векторов
 	{
 		cout << "Vector#" << i + 1 << endl;
@@ -40,28 +40,27 @@ int main()
 		vectx.push_back(buffx);
 	}
 
-	unsigned int vector_size = buffx.size();
 	int num = 1;
-	for (int i = 2; i < n * 2 + 1; i = i + 2) {
+	for (int i = 2; i < n * 2 + 1; i = i + 2) // Цикл вывода координат векторов 
+	{
 		cout << "Vector #" << num << " = (" << vectx[i-1] << ";" << vectx[i] << ")" << endl;
 		num++;
 	}
 	cout << "--------------------------------" << endl;
 	double max = 0;
-	int count;
-	char a[100];
-	for (int i = 2; i < n * 2 + 1; i = i + 2)
+	int count; // Здесь будет храниться порядковый номер вектора с максимальной длиной
+	for (int i = 2; i < n * 2 + 1; i = i + 2) // Цикл подсчёта длины каждого вектора
 	{
 		int x = stoi(vectx[i-1]);
 		int y = stoi(vectx[i]);
-		if (length(x, y) > max)
+		if (length(x, y) > max) // Нахождение среди них максимально длинного вектора
 		{
 			max = length(x, y);
 			count = i / 2;
 
 		}
 	}
-	cout << "Vector #" << count << " has max length = " << max << endl;
+	cout << "Vector #" << count << " has max length = " << max << endl; // Вывод 
 	_getch();
 	return 0;
 }
